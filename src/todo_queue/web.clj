305aa5c31dc8a18de21dcc76e6355b607f005d07
Otto Jursch <ottojursch@gmail.com>
2015-09-06 13:20:37 -0700

    fixed web.clj todo-wueue

--- src/todo_queue/web.clj
+++ src/todo_queue/web.clj
@@ -1,11 +1,11 @@
-(ns to-do-queue.web
+(ns todo-queue.web
   (:require [compojure.core :refer  [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
             [clojure.java.io :as io]
             [rind.adapter.jetty :as jetty]
             [environ.core :refer [env]]
-            [to-do-queue.handler :as handler]))
+            [todo-queue.handler :as handler]))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
