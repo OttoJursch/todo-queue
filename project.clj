b3f7ffc09f6d19cff480094fbd78de46a94cf600
Otto Jursch <ottojursch@gmail.com>
2015-09-06 16:06:33 -0700

    added postgres and json

--- project.clj
+++ project.clj
@@ -7,7 +7,10 @@
                  [ring/ring-defaults "0.1.2"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [environ "1.0.0"]
-                 [crypto-password "0.1.3"]]
+                 [crypto-password "0.1.3"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [postgresql "9.1-901-1.jdbc4"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler todo-queue.handler/app}
   :uberjar-name "todo-queue-standalone.jar"
--- src/todo_queue/handler.clj
+++ src/todo_queue/handler.clj
@@ -3,25 +3,36 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.response :as response]
-            [crypto.password.scrypt :as password]))
+            [crypto.password.scrypt :as password]
+            [clojure.java.jdbc :as db]
+            [clojure.data.json :as json]))
 
 (defn password-check [id password]
   (println "Posting up " id " " password)
   ;; Via sql get password for provided id
   ;;(if (= (password/decrypt from-sql) password)
-  (response/redirect "")
+  (if (= password (password/decrypt (get (db/query ["SELECT password FROM todo_queue_users WHERE email = ?" id]) :password)))
+    (response/redirect "password-is-good")
+  (response/redirect "Incorrect username or password"))
   ;;(response/redirect "bad username or password"))
+)
+
+(defn account-create-success [id password]
+  (db/insert (env :database_url "postgres://localhost:5432") :todo_queue_users {:email id :password password})
+  (response/redirect "password-is-good")
 )
 
 (defn create-account [id password]
   ;First check if email account already exists
   ;If it doesn't, pass (password/encrypt password) and email to sql database
-  (response/redirect "/home.html")
+  (if (= "" (get (db/query ["SELECT email FROM todo_queue_users WHERE email = ?" id]) :email))
+  (account-create-success id password)     
+  (response/redirect "Account Already Exists"))
 ) 
 
 (defroutes app-routes
   (GET "/" [] (response/redirect "index.html"))
-  (GET "/login/:id/:password" [id password] "sword-is-good");(password-check id password))
+  (GET "/login/:id/:password" [id password] (password-check id password))
   (GET "/signup/:id/:password" [id password] (create-account id password))
   (route/not-found "Not Found"))
 
